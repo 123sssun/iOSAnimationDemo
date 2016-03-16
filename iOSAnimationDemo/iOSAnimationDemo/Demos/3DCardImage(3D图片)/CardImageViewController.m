e489c0663c21a75a65d98bcd0034eede67ea6ba6
Jack Zeng <zengtian1211@163.com>
2016-03-16 23:26:19 +0800

    3D动画视图+注释

--- iOSAnimationDemo/iOSAnimationDemo/Demos/3DCardImage(3D图片)/CardImageViewController.m
+++ iOSAnimationDemo/iOSAnimationDemo/Demos/3DCardImage(3D图片)/CardImageViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CardImageViewController.h"
+#import "ThreeDView.h"
 
 @interface CardImageViewController ()
 
@@ -20,7 +21,9 @@
     CGFloat x = random() % 254 + 1.0;
     self.view.backgroundColor = [UIColor colorWithRed:x / 255.0 green:255.0 / 255.0 blue:255.0 / 255.0 alpha:1.0];
     
-    NSLog(@"哈哈");
+    ThreeDView *threeDView = [[ThreeDView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width/2, self.view.bounds.size.height/2)];
+    threeDView.center = self.view.center;
+    [self.view addSubview:threeDView];
 }
 
 - (void)didReceiveMemoryWarning {
