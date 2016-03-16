22dec210264744c8f2f4389ef80078296c0c8e57
JustOneLastDance <xss186@126.com>
2016-03-16 14:21:07 +0800

    改变背景颜色

--- iOSAnimationDemo/iOSAnimationDemo/Demos/RubberBall(小皮球)/RubberBallViewController.m
+++ iOSAnimationDemo/iOSAnimationDemo/Demos/RubberBall(小皮球)/RubberBallViewController.m
@@ -17,23 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    CGFloat x = random() % 254 + 1.0;
-    self.view.backgroundColor = [UIColor colorWithRed:x / 255.0 green:x / 255.0 blue:x / 255.0 alpha:1.0];
+//    CGFloat x = random() % 254 + 1.0;
+//    self.view.backgroundColor = [UIColor colorWithRed:x / 255.0 green:x / 255.0 blue:x / 255.0 alpha:1.0];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
