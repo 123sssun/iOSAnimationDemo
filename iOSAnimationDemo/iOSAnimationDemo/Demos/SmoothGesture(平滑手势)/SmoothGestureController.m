a718f1c2e3dc2efb6e6e7ced72bdaf37409c79cb
Geeven <598800360@qq.com>
2016-03-17 21:40:58 +0800

    完成效果

--- iOSAnimationDemo/iOSAnimationDemo/Demos/SmoothGesture(平滑手势)/SmoothGestureController.m
+++ iOSAnimationDemo/iOSAnimationDemo/Demos/SmoothGesture(平滑手势)/SmoothGestureController.m
@@ -6,9 +6,16 @@
 //  Copyright © 2016年 Mekingo8. All rights reserved.
 //
 
+#define screenSize [UIScreen mainScreen].bounds.size
 #import "SmoothGestureController.h"
+#import "SmoothView.h"
 
-@interface SmoothGestureController ()
+@interface SmoothGestureController (){
+    
+    UIImageView * _imageView;
+    
+    CGPoint _tmpPoint;
+}
 
 @end
 
@@ -16,25 +23,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    CGFloat x = random() % 254 + 1.0;
-    self.view.backgroundColor = [UIColor colorWithRed:x / 255.0 green:255.0 / 255.0 blue:255.0 / 255.0 alpha:1.0];
+    
+    //    CGFloat x = random() % 254 + 1.0;
+    //    self.view.backgroundColor = [UIColor colorWithRed:x / 255.0 green:255.0 / 255.0 blue:255.0 / 255.0 alpha:1.0];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.title = @"平滑手势效果";
+    
+    SmoothView * imageView = [[SmoothView alloc] initWithImage:[UIImage imageNamed:@"SmoothGesture01"]];
+    imageView.center = self.view.center;
+    imageView.bounds = CGRectMake(0, 0, 250, 150);
+    imageView.gestureView = self.view;
+    
+    //设置毛玻璃效果
+    UIVisualEffectView * effectView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark]];
+    [self.view addSubview:effectView];
+    effectView.frame = self.view.bounds;
+    [effectView.contentView addSubview:imageView];
     
 }
 
+
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
